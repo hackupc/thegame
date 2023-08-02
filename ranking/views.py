0d081ac3ecb9c16a9873d24baffdcec1a81fbcd0
EncryptEx <limusina10@gmail.com>
2023-08-02 12:41:52 +0200

    Added documentation

--- ranking/views.py
+++ ranking/views.py
@@ -17,19 +17,24 @@ class RankingView(LoginRequiredMixin, SingleTableView):
             .annotate(count=Count('*'), time=Max('last_try')).order_by('-count', 'time')
     
 def get_chart(request):
+    # Get all Successful attempts
+    # TODO: limit to 10 players
     allEntries = list(ChallengeUser.objects.filter(success=True).values('user__username', 'last_try', 'total_attempts').order_by('last_try'))
 
     players = {}
     Pcounter = {}
+    # Classify them by player, in chronological order.
     for successes in allEntries:
         Pcounter[successes['user__username']] = Pcounter.get(successes['user__username'],0)+1
         playerDb =  players.get(successes['user__username'],[])
         playerDb.append({"x": successes['last_try'],"y": Pcounter[successes['user__username']]})
         players[successes['user__username']] = playerDb
     
+    # Format the output required from ChartJS in order to work 
     final = []
     for username in players:
         final.append({
+            # Set dataset name to the username owner
             "label" : username,
             "data": players[username]
         })
