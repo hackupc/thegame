56af12bf4f28273f752896f5a2a236426ce4b780
EncryptEx <limusina10@gmail.com>
2023-08-05 12:53:44 +0200

    added cache to chart

--- ranking/urls.py
+++ ranking/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
 from ranking.views import RankingView, ChartView
+from django.views.decorators.cache import cache_page
 
 urlpatterns = [
     path('', RankingView.as_view(), name='ranking'),
-    path('chart',  ChartView.as_view(), name="chart")
+    path('chart',  cache_page(60*60)(ChartView.as_view()), name="chart")
 ]
